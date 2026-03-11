7a5af7abf5c707771a2dee5d521792fb48424b54
isabelaviana-source <isabela.viana@ufvjm.edu.br>
2026-03-11 10:35:38 -0300

    Create requirements.R file with list of R packages for DoE analysis system

---
+++ requirements.R
@@ -0,0 +1,13 @@
+# Required R Packages for DoE Analysis System
+
+The following R packages are required for the Design of Experiments (DoE) analysis system:
+
+- DBI
+- RPostgres
+- tidyverse
+- lme4
+- rsm
+- FrF2
+- ggplot2
+
+Please ensure that these packages are installed before running the analyses.
